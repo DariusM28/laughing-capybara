f172af77f941e3ec5709fb7f6a87a665b0b92dff
DariusM28 <104643225+DariusM28@users.noreply.github.com>
2023-04-11 04:43:37 +0000

    Pending changes exported from your codespace

---
+++ problema 1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Ingrese su Nombre");
+           string nombre= Console.ReadLine();
+           
+           Console.WriteLine("Su nombre es"+nombre);
+            Console.ReadKey();
+        }
+    }
+}
